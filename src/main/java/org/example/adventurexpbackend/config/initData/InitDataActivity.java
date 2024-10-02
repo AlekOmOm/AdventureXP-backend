ec5151b50c95e8c4f7f4a257f1ec4d9f70696dd4
Aleksander Hvid Ellegård <142972647+AlekOmOm@users.noreply.github.com>
2024-10-02 13:34:59 +0200

    Add equipment data initialization and repository save
    Injected InitDataEquipment and ActivityRepository dependencies, and expanded activity initialization with relevant equipment lists and requirements. Updated saveData method to persist activities and equipment data correctly.

--- src/main/java/org/example/adventurexpbackend/config/initData/InitDataActivity.java
+++ src/main/java/org/example/adventurexpbackend/config/initData/InitDataActivity.java
@@ -1,6 +1,9 @@
 package org.example.adventurexpbackend.config.initData;
 
 import org.example.adventurexpbackend.model.Activity;
+import org.example.adventurexpbackend.model.Equipment;
+import org.example.adventurexpbackend.repository.ActivityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.ArrayList;
@@ -12,13 +15,29 @@ public class InitDataActivity {
 
     // constructor parameters: String name, String description, int pricePrPerson, int timeMaxLimit, int ageMin, int ageMax, int personsMin, int personsMax, LocalTime openingTime, LocalTime closingTime, int timeSlotInterval, List<Equipment> equipmentList, Set<Equipment> equipmentRequiredPerPerson) {
 
+    private final InitDataEquipment initDataEquipment;
+    private final ActivityRepository activityRepository;
+
+    @Autowired
+    public InitDataActivity(InitDataEquipment initDataEquipment, ActivityRepository activityRepository) {
+        this.initDataEquipment = initDataEquipment;
+        this.activityRepository = activityRepository;
+    }
+
+
     protected List<Activity> getActivities() {
         return new ArrayList<>(List.of(
-                new Activity("Paintball", "Paintball is a fun activity for everyone", 100, 120, 10, 100, 2, 20, 10, 18, 2, null, null),
+                new Activity("Paintball", "Paintball is a fun activity for everyone", 100, 120, 10, 100, 2, 20, 10, 18, 2, initDataEquipment.getPaintBallEquipmentList(), initDataEquipment.getPaintBallEquipmentRequiredPerPerson()),
+                new Activity("Climbing", "Climbing is a fun activity for everyone", 100, 120, 10, 100, 2, 20, 10, 18, 2, initDataEquipment.getClimbingEquipmentList(), initDataEquipment.getClimbingEquipmentRequiredPerPerson()),
+                new Activity("Go-kart", "Go-kart is a fun activity for everyone", 100, 120, 10, 100, 2, 20, 10, 18, 2, initDataEquipment.getGoKartEquipmentList(), initDataEquipment.getGoKartEquipmentRequiredPerPerson())
         ));
     }
 
     public void saveData() {
-        // activityRepository.saveAll(getActivities());
+        activityRepository.saveAll(getActivities());
+        initDataEquipment.saveData(getActivities());
+
     }
+
+
 }
