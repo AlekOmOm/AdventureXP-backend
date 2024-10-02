21fdf6cd7bc94151d6c539f336420fdd44c6bd8a
Magnus Munk Braunstein <147395199+MagnusMunkBraunstein@users.noreply.github.com>
2024-10-02 12:01:00 +0200

    Putmmaping to endpoint where equipment is sat as functional

---
+++ src/main/java/org/example/adventurexpbackend/controller/EquipmentController.java
@@ -0,0 +1,30 @@
+package org.example.adventurexpbackend.controller;
+
+
+import org.example.adventurexpbackend.service.EquipmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EquipmentController {
+
+    @Autowired
+    EquipmentService equipmentService;
+
+
+    // this is the endpoint to mark equipment as functional by the equipment id
+    @PutMapping("/markAsFunctional/{id}")
+    public ResponseEntity<String> markAsFunctional(@PathVariable long id) {
+        String function = equipmentService.markAsFunctional(id);
+
+        if (function.contains("error")){
+            return ResponseEntity.badRequest().body(function);
+        }
+        return ResponseEntity.ok(function);
+    }
+
+}
