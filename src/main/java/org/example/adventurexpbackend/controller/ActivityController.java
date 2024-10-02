a5be86b3ccc34f378933a651fe483b66bac036cf
MatDalleBalle <mada0003@stud.kea.dk>
2024-10-02 15:11:34 +0200

    feat: Update base URL for ActivityController endpoints
    - Changed the base URL in `ActivityController` to `/Activity`

--- src/main/java/org/example/adventurexpbackend/controller/ActivityController.java
+++ src/main/java/org/example/adventurexpbackend/controller/ActivityController.java
@@ -1,4 +1,12 @@
 package org.example.adventurexpbackend.controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Activity")
 public class ActivityController {
+
+
 }
