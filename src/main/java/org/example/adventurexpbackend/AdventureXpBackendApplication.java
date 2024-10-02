eb08c37ffb40547de2e776722a1af3ddd37d7e63
Aleksander Hvid Ellegård <142972647+AlekOmOm@users.noreply.github.com>
2024-10-02 09:47:16 +0200

    init

---
+++ src/main/java/org/example/adventurexpbackend/AdventureXpBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.adventurexpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureXpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdventureXpBackendApplication.class, args);
+    }
+
+}
