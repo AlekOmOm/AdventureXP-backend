fb5377e7f8e0006f59405daab9117307eb47a3f1
Aleksander Hvid Ellegård <142972647+AlekOmOm@users.noreply.github.com>
2024-10-02 12:29:58 +0200

    Add configuration classes for RestTemplate and InitData
    Introduce `RestTemplate` class and `InitData` component to the configuration package. `RestTemplate` class is currently empty, intended for HTTP client configuration, while `InitData` implements `CommandLineRunner` for initializing data at application startup.

---
+++ src/main/java/org/example/adventurexpbackend/config/RestTemplate.java
@@ -0,0 +1,4 @@
+package org.example.adventurexpbackend.config;
+
+public class RestTemplate {
+}
---
+++ src/main/java/org/example/adventurexpbackend/config/initData/InitData.java
@@ -0,0 +1,18 @@
+package org.example.adventurexpbackend.config.initData;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InitData implements CommandLineRunner {
+
+    // services for saving data
+    // private final
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+
+
+}
