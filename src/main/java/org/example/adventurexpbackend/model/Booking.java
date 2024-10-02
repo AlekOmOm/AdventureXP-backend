19e938e7d9a581eeb23eb66a1c1872c7d9128575
kasperkhansen <kasper1999.hansen@gmail.com>
2024-10-02 11:19:39 +0200

    added default constructor to Booking model class and getters and setters

--- src/main/java/org/example/adventurexpbackend/model/Booking.java
+++ src/main/java/org/example/adventurexpbackend/model/Booking.java
@@ -1,10 +1,69 @@
 package org.example.adventurexpbackend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalTime;
+import java.util.List;
 
 @Entity
 public class Booking {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private List<Equipment> equipmentList;
+    private int personsAmount;
+
+    @ManyToOne
+    @JoinColumn(name = "activity", referencedColumnName = "id")
+    Activity activity;
+
+    //Default constructor
+    public Booking() {}
+
+
+
+//----------------------getters and setters------------------------
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public List<Equipment> getEquipmentList() {
+        return equipmentList;
+    }
+
+    public void setEquipmentList(List<Equipment> equipmentList) {
+        this.equipmentList = equipmentList;
+    }
+
+    public int getPersonsAmount() {
+        return personsAmount;
+    }
+
+    public void setPersonsAmount(int personsAmount) {
+        this.personsAmount = personsAmount;
+    }
 }
