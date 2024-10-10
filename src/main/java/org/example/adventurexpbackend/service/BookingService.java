ca7724823fd4ab23d5e7ada41a03600d4d5cd546
Magnus Munk Braunstein <147395199+MagnusMunkBraunstein@users.noreply.github.com>
2024-10-10 08:39:26 +0200

    Nothing

--- src/main/java/org/example/adventurexpbackend/service/BookingService.java
+++ src/main/java/org/example/adventurexpbackend/service/BookingService.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+
 import org.example.adventurexpbackend.controller.dto.AvailableTimeSlot;
 import org.springframework.web.client.RestTemplate;
 
